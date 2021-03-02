d3876e9368c1bdbbf8a1cd89a2077c8ec92f1089
CodeMonkey <tremainetan@gmail.com>
2021-03-03 07:28:43 +0800

    Added StringGenerator Module to generate read string from object detection

---
+++ StringGenerator.py
@@ -0,0 +1,31 @@
+import cmudict
+
+def use_an(word, pronunciations=cmudict.dict()):
+    for syllables in pronunciations.get(word, []):
+        return syllables[0][-1].isdigit()
+
+def generate_read_string(objects):
+    returnString = "The image contains "
+    for object, number in objects.items():
+        if number > 1:
+            lastChar = object[-1]
+            addedString = str(number) + " " + object
+            if lastChar == 's':
+                addedString += "es"
+            else:
+                addedString += "s"
+            index = list(objects.keys()).index(object)
+        else:
+            if use_an(object):
+                addedString = "an " + object
+            else:
+                addedString = "a " + object
+        index = list(objects.keys()).index(object) + 1
+        if index != len(objects):
+            #  Not Last Item
+            if index == (len(objects) - 1):
+                addedString += ", and "
+            else:
+                addedString += ", "
+        returnString += addedString
+    return returnString
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import ObjectDetection
+import StringGenerator
 
 def main():
     objects = ObjectDetection.scan_image('images/dining_table.jpg', 'yololib', True)
+    readString = StringGenerator.generate_read_string(objects)
+    print(readString)
 
 if __name__ == "__main__": main()
