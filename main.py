182bf4cd4b1f33ef7dad570b76ad3a67553bed47
CodeMonkey <tremainetan@gmail.com>
2021-03-03 07:29:59 +0800

    Added SpeakOut Module to convert generated text to speech

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import ObjectDetection
 import StringGenerator
+import SpeakOut
 
 def main():
     objects = ObjectDetection.scan_image('images/dining_table.jpg', 'yololib', True)
     readString = StringGenerator.generate_read_string(objects)
     print(readString)
+    SpeakOut.speak_text(readString)
 
 if __name__ == "__main__": main()
