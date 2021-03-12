b0a07fb5adeb4a94f7c1ad8a6afc985186ad14e1
ash15khng <ash15khng@gmail.com>
2021-03-12 22:23:46 +0800

    Added button function (raspberry pi only)

--- main.py
+++ main.py
@@ -3,30 +3,43 @@ import strgen
 import ocr
 import tts
 import os
+import RPi.GPIO as GPIO
 
 def main():
-    path = "images/TextAndObjects.png"
 
-    if not (file_exists(path)):
-        print(">>>File Does not Exist...")
-    else:
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-        #Scanning Image
-        image = objectdetect.get_image(path)
-        ocrText = ocr.get_text(image)
-        objectData = objectdetect.scan_image(image, "yololib")
-        processedImage = objectData['processed_image']
-        objects = objectData['objects']
-        objectText = strgen.generate_read_string(objects)
+    running = False
 
-        #Speaking Out Data
-        tts.speak_text("PRINTING TEXT IN IMAGE...")
-        tts.speak_text(ocrText)
+    while not running:
+        if GPIO.input(15) == GPIO.HIGH:
+            running = True
+            path = "images/TextAndObjects.png"
 
-        tts.speak_text("PRINTING OBJECTS IN IMAGE...")
-        tts.speak_text(objectText)
+            if not (file_exists(path)):
+                print(">>>File Does not Exist...")
+            else:
 
-        objectdetect.show_image(processedImage)
+                #Scanning Image
+                image = objectdetect.get_image(path)
+                ocrText = ocr.get_text(image)
+                objectData = objectdetect.scan_image(image, "yololib")
+                processedImage = objectData['processed_image']
+                objects = objectData['objects']
+                objectText = strgen.generate_read_string(objects)
+
+                #Speaking Out Data
+                tts.speak_text("PRINTING TEXT IN IMAGE...")
+                tts.speak_text(ocrText)
+
+                tts.speak_text("PRINTING OBJECTS IN IMAGE...")
+                tts.speak_text(objectText)
+
+                objectdetect.show_image(processedImage)
+
+            running = False
 
 def file_exists(file):
     if os.path.exists(file):
